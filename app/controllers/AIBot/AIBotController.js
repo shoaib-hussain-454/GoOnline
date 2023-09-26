8e3de1f0737ddb20bf2fbd2f5ae907e093888a3f
shoaib.husain <shoaib.husain@infobeans.com>
2023-09-26 18:54:18 +0530

    AIBot Test

--- app/controllers/AIBot/AIBotController.js
+++ app/controllers/AIBot/AIBotController.js
@@ -1,11 +1,18 @@
 const { asyncWrapper, apiResponse } = require("../../helper");
 // const { TaskModel } = require("../../models/index")
-const gTTS = require("gtts")
+const gTTS = require("gtts");
+const fs = require("fs");
 
 exports.textToSpeech = [
     asyncWrapper(async (req, res) => {
         const { speech } = req.body;
         const  gtts = new gTTS(speech, 'en');
+
+        fs.unlink('./uploads/Voice.mp3',function(err){
+            if(err) return console.log(err);
+            console.log('file deleted successfully');
+       });  
+
         await gtts.save('uploads/Voice.mp3');
         let Data = { audioLink: "Voice.mp3"}
         return apiResponse.succesReponseWithData(res, `Successfully`, Data)
